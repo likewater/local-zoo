ecdce339337469839ea1e438fd775ef3828771ef
likewater <lakebayomackway@gmail.com>
2017-05-07 19:53:45 -0700

    add refactoring

--- app/app.component.ts
+++ app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
   selectedAnimal: null;
 
   masterAnimalList: Animal[] = [
-    new Animal('Artic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool Shade', 'Loud Noises'),
+    new Animal('Artic Fox', 'Moon', 3, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool Shade', 'Loud Noises'),
     new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forect Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope based'),
     new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
   ];
